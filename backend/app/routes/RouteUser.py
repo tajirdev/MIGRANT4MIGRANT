d1558978d5128d552ed45ed9d7ebb55665f94898
tajirdev <mgenialfred671@gmail.com>
2026-05-01 23:50:34 +0300

    puting user reg,modify user table delete some test files

---
+++ backend/app/routes/RouteUser.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.schemas import schemaUser
+from app.services import Userservice
+from typing import List
+
+router = APIRouter(
+    tags=['register'],
+    prefix="/auth"
+    
+)
+
+
+# routes for reg
+@router.post('/register',response_model=schemaUser.showUser)
+def register(request:schemaUser.User,db:Session=Depends(get_db)):
+    return Userservice.registerUser(request,db)
+
+
+
+@router.get('/user',response_model=List[schemaUser.showUser])
+def getuser(db:Session=Depends(get_db)):
+    return Userservice.getuser_all(db)
