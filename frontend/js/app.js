eddbef95af9708b066104103853f5dc0b8c99994
tajirdev <mgenialfred671@gmail.com>
2026-05-29 16:23:29 +0300

    adding token removal

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -122,8 +122,41 @@ function requireAuth() {
   }
     
 }
+function checkTokenExpiry() {
+    const token = localStorage.getItem('pa_token'); 
 
-// Logout function
+    if (!token) {
+
+        window.location.href = 'index.html'; 
+        return;
+    }
+
+    try {
+       
+        const payloadBase64 = token.split('.')[1];
+        
+        
+        const decodedJson = atob(payloadBase64);
+        const payload = JSON.parse(decodedJson);
+
+       
+        const currentTime = Math.floor(Date.now() / 1000);
+
+        if (currentTime >= payload.exp) {
+            console.warn("Session expired. Redirecting to login...");
+            localStorage.removeItem('pa_token');
+            window.location.href = 'index.html';
+        }
+    } catch (error) {
+        console.error("Invalid token format. Redirecting...");
+        localStorage.removeItem('pa_token');
+        window.location.href = '/login.html';
+    }
+}
+
+
+checkTokenExpiry();
+
 function logout() {
   localStorage.removeItem('pa_token');
   localStorage.removeItem('pa_email');
